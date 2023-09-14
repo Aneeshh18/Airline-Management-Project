1081c8259709f40866b4f5160ea03a5352d9eda9
Aneesh Sahu <88325646+Aneeshh18@users.noreply.github.com>
2023-09-14 17:36:05 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,21 +6,49 @@ class CityRepository {
             const city = await City.create({name});
             return city;
         } catch(error){
+            console.log("Something went wrong in the respository layer");
             throw {error};
         }
     }
 
-    async dleteCity(cityid){
+    async deleteCity(cityid){
         try{
             const city = await City.destroy({
                 where:{
                     id:cityId
                 }
             });
+            return true;
         } catch(error){
+            console.log("Something went wrong in the respository layer");
             throw {error};
         }
     }
+
+    async updateCity(cityId, data){
+        try{
+            const city = await City.update(data, {
+                where:{
+                    cityid
+                }
+            });
+            return city;
+        }catch(error){
+            console.log("Something went wrong in the respository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId){
+        try{
+            const city = await City.findByPk(cityid);
+            return city;
+        }catch(error){
+            console.log("Something went wrong in the respository layer");
+            throw {error};
+        }
+    }
+
 }
 
 
