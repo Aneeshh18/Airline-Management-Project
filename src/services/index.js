66a68834fce117219c9710608c16a8a87957cdab
Aneesh Sahu <88325646+Aneeshh18@users.noreply.github.com>
2023-09-24 12:23:26 +0530

    Added crud classes for basic crud operations

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,6 @@
 module.exports = {
     CityService: require('./city-Service'),
-    FlightService: require('./flight-Service')
+    FlightService: require('./flight-Service'),
+    AirportService: require('./airport-Service'),
+    CrudService: require('./crud-Service'),
 }
